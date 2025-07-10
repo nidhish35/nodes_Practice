fffca6bc2410054dcb88f600c053803a8ef48cdf
nidhish35 <malavnidhish35@gmail.com>
2025-07-10 16:36:49 +0530

    http server creation

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const fs = require('fs');
+
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now()} : New request received\n`;
+    fs.appendFile('log.txt', log, (err, data) => {
+        res.end("Hello from the server!");
+    });
+});
+
+myServer.listen(8000, () => {
+    console.log("Server is running on port 8000");
+});
