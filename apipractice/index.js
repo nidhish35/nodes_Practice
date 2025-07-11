2d3a9389e84d5f982c7a6139e16b34233ae4d1e6
nidhish35 <malavnidhish35@gmail.com>
2025-07-11 07:55:12 +0530

    api practice combining routes

--- apipractice/index.js
+++ apipractice/index.js
@@ -4,33 +4,40 @@ const app = express();
 const port = 8000;
 
 // Routes / if api/users is requested, return the json file if not api/users, return a html file
-app.get('/api/users', (req, res) => {             
+app.get('/api/users', (req, res) => {
     res.json(users);
 });
 
-app.get('/api/users/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const user = users.find(u => u.id === id);
-    
-    if (user) {
-        res.json(user);
-    } else {
-        res.status(404).send('User not found');
-    }
-})
+app.route('/api/users/:id')
+    .get((req, res) => {
+        const id = parseInt(req.params.id, 10);
+        const user = users.find(u => u.id === id);
+
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).send('User not found');
+        }
+    })
+    .patch((req, res) => {
+        return res.json({ status: 'Pending' })
+    })
+    .delete((req, res) => {
+        return res.json({ status: 'Pending' })
+    });
 
 
 app.post('/api/users', (req, res) => {
-    return res.json({status : 'Pending'})
+    return res.json({ status: 'Pending' })
 })
 
-app.patch('/api/users/:id', (req, res) => {
-    return res.json({status : 'Pending'})
-})
+// app.patch('/api/users/:id', (req, res) => {
+//     return res.json({ status: 'Pending' })
+// })
 
-app.delete('/api/users/:id', (req, res) => {
-    return res.json({status : 'Pending'})
-})
+// app.delete('/api/users/:id', (req, res) => {
+//     return res.json({ status: 'Pending' })
+// })
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
